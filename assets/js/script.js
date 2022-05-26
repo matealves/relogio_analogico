95fbdf7ada50ccc424bf2d11d091ee367bce807d
Mateus Alves <contatomateusalves@hotmail.com>
2022-05-26 10:48:44 -0300

    version 1.0 - Dark Mode

---
+++ assets/js/script.js
@@ -0,0 +1,85 @@
+const hoursE1 = document.getElementById("hour");
+const minutesE1 = document.getElementById("minute");
+const secondsE1 = document.getElementById("second");
+// const toggle = document.getElementById("toggle");
+const dateE1 = document.getElementById("date");
+const timeE1 = document.getElementById("time");
+
+const toggle = document.getElementById("toggle2");
+
+toggle.addEventListener("click", () => {
+  const html = document.querySelector("html");
+  const mode = document.querySelector(".mode");
+  if (html.classList.contains("light")) {
+    html.classList.remove("light");
+    html.classList.add("dark");
+    mode.innerHTML = "Light Mode";
+    toggle.classList.toggle("active");
+  } else {
+    html.classList.remove("dark");
+    html.classList.add("light");
+    mode.innerHTML = "Dark Mode";
+    toggle.classList.toggle("active");
+  }
+});
+
+//BOTÃO SIMPLES
+// toggle.addEventListener("click", () => {
+//   const html = document.querySelector("html");
+//   if (html.classList.contains("dark")) {
+//     html.classList.remove("dark");
+//     toggle.innerHTML = "Dark Mode";
+//   } else {
+//     html.classList.add("dark");
+//     toggle.innerHTML = "Light Mode";
+//   }
+// });
+
+const days = [
+  "Domingo",
+  "Segunda",
+  "Terça",
+  "Quarta",
+  "Quinta",
+  "Sexta",
+  "Sábado",
+];
+const months = [
+  "Jan",
+  "Fev",
+  "Mar",
+  "Abr",
+  "Mai",
+  "Jun",
+  "Jul",
+  "Ago",
+  "Set",
+  "Out",
+  "Nov",
+  "Dez",
+];
+
+setInterval(() => {
+  const time = new Date();
+  const day = time.getDay();
+  const date = time.getDate();
+  const month = time.getMonth();
+  const hour = time.getHours();
+  const minutes = time.getMinutes();
+  const seconds = time.getSeconds();
+  // const hourIn12Hour = hour % 12;
+  const ampm = hour > 12 ? "PM" : "AM";
+
+  dateE1.innerHTML = `${days[day]}, ${date} ${months[month]}`;
+  timeE1.innerHTML = `${hour}:${
+    minutes < 10 ? `0${minutes}` : minutes
+  } ${ampm}`;
+
+  hoursE1.style.transform = `translate(-50%, -100%) rotate(${hour * 30}deg)`;
+  minutesE1.style.transform = `translate(-50%, -100%) rotate(${
+    minutes * 6
+  }deg)`;
+  secondsE1.style.transform = `translate(-50%, -100%) rotate(${
+    seconds * 6
+  }deg)`;
+}, 1000);
